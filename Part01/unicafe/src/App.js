7d26811d5f7529db3b66f88f39d9f2be4395cc3b
Nick Paschalis <lesrevenants1967@gmail.com>
2021-10-11 12:18:51 +0300

    1.8 & added state functionality - no more hardcoded values

--- Part01/unicafe/src/App.js
+++ Part01/unicafe/src/App.js
@@ -1,31 +1,49 @@
 import React, { useState } from 'react';
 
+const Statistics = props => {
+  let all = props.good + props.neutral + props.bad;
+  let average = (props.good - props.bad) / all;
+  let positive = (props.good / all) * 100;
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const good = 6;
-  const neutral = 2;
-  const bad = 1;
-  let all = good + neutral + bad;
-  let average = (good - bad) / all;
-  let positive = (good / all) * 100;
+  const goodClickHandler = () => {
+    console.log('clicked');
+    setGood(good + 1);
+  };
+
+  const neutralClickHandler = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const badClickHandler = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <button onClick={goodClickHandler}>good</button>
+      <button onClick={neutralClickHandler}>neutral</button>
+      <button onClick={badClickHandler}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
