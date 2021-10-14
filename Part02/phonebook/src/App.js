ea229c94d75e301b4e07629a528e4770de6a92dd
Nick Paschalis <lesrevenants1967@gmail.com>
2021-10-14 18:53:10 +0300

    2.6

---
+++ Part02/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addContact = event => {
+    event.preventDefault();
+    setPersons(persons.concat([{ name: newName }]));
+    setNewName('');
+  };
+
+  const nameChangeHandler = event => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={nameChangeHandler} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
