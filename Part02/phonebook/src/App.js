292b4d3631f2c4e4c5706b93a65393678d7c2b92
Nick Paschalis <lesrevenants1967@gmail.com>
2021-10-15 15:19:55 +0300

    2.11

--- Part02/phonebook/src/App.js
+++ Part02/phonebook/src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addContact = event => {
     event.preventDefault();
